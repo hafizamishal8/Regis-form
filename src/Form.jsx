cbcbbeb98a4132710473ddf48e9a683acc04d519
mishal <hafizamishal8@gmail.com>
2024-04-03 16:59:38 +0500

    Initial commit

---
+++ src/Form.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import './Form.css'
+
+const Form = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    dateOfBirth: null
+  });
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    const val = type === 'checkbox' ? checked : value;
+    setFormData({
+      ...formData,
+      [name]: val
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    // here the conversion of object into  an array
+    const formDataArray = Object.entries(formData);
+   console.log(formDataArray);
+    
+  };
+
+return (
+    <div className="form-container">
+      <h2>Registration Form</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>First Name:</label>
+          <input
+            type="text"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Last Name:</label>
+          <input
+            type="text"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </div>
+       
+        <div className="form-group">
+          <label>Date of Birth:</label>
+          <input
+            type="date"
+            name="dateOfBirth"
+            value={formData.dateOfBirth}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
